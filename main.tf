0a8a757da9f17728dff1972bbbdd8b2e7998eb4a
Renato J Ramos <rjramos70@gmail.com>
2023-07-21 10:38:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,29 +6,25 @@ provider "aws" {
 
 # Defining a IAM group with name 'app_ami'
 data "aws_ami" "app_ami" {
-  most_recent  =  true
+  most_recent = true
 
-  # Define filters
   filter {
-    name    = "name"
-    values  =  ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-  
-  filter {
-    name    = "virtualization-type"
-    values  =  ["hvm"]
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
-  owners  =  ["979382823631"]  # Bitnami
-  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
-
-# Define a S3 resource
 resource "aws_instance" "blog" {
-  ami            =  data.aws_ami.app_ami.id
-  instance_type  =  var.instance_type
+  ami           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 
-  tags  =  {
-    Name  =  "Learning Terraform"
+  tags = {
+    Name = "Learning Terraform"
   }
 }
